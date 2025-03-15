74ce3a1072364e58b355d66777077750841c0b11
Ahsaan Majeed <ahsaanmaj8@gmail.com>
2025-03-15 20:58:05 +0100

    Fix url!

--- frontend/Amb Engineering Company/src/Component/utils/utils.js
+++ frontend/Amb Engineering Company/src/Component/utils/utils.js
@@ -1,2 +1,2 @@
 // export const Backend_Url = "http://localhost:8086/api/userEmail" 
-export const Backend_Url = "https://amb-engineering-company.onrender.com" 
+export const Backend_Url = "https://amb-engineering-company.onrender.com/api/userEmail" 
