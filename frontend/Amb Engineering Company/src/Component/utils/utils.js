25da4baf11fb4600b28293e26fb8984102b4f244
Ahsaan Majeed <ahsaanmaj8@gmail.com>
2025-03-15 20:27:31 +0100

    Fix Solved!

--- frontend/Amb Engineering Company/src/Component/utils/utils.js
+++ frontend/Amb Engineering Company/src/Component/utils/utils.js
@@ -1,1 +1,2 @@
-export const Backend_Url = "http://localhost:8086/api/userEmail" 
+// export const Backend_Url = "http://localhost:8086/api/userEmail" 
+export const Backend_Url = "https://amb-engineering-company.onrender.com" 
