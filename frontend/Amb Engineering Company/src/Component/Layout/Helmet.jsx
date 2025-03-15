9f099717c4f18bb127205fb5c783c7618fa02f5c
Ahsaan Majeed <ahsaanmaj8@gmail.com>
2025-03-15 22:06:39 +0100

    Fix Seo again!

--- frontend/Amb Engineering Company/src/Component/Layout/Helmet.jsx
+++ frontend/Amb Engineering Company/src/Component/Layout/Helmet.jsx
@@ -7,6 +7,8 @@ const HelmetLayout = ({
   description = "In this company you can cut piece from Laser Cutting Machine",
   keywords = "laser cutting,amb engineering,fiber laser cut,gate design laser cut",
   author = "Farhan Majeed",
+  ogImage = "https://images.pexels.com/photos/29988963/pexels-photo-29988963/free-photo-of-industrial-laser-cutter-in-factory-setting.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",  // Add a default og:image
+  ogUrl = "https://amb-engineering-company.vercel.app",  // Add a default og:url
 }) => {
   return (
     <>
@@ -16,12 +18,21 @@ const HelmetLayout = ({
         <meta name="keywords" content={keywords} />
         <meta name="author" content={author} />
         <title>{title}</title>
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:url" content={ogUrl} />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-      <div>
-        {
-            children
-        }
-      </div>
+      <div>{children}</div>
     </>
   );
 };
