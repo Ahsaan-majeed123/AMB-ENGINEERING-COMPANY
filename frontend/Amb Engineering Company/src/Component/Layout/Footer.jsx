cba4385609a107e8028d304454fbd186b5824cb6
Ahsaan Majeed <ahsaanmaj8@gmail.com>
2025-03-19 09:00:25 +0100

    Site map  and robot file and canonical(dynamic) and footer image updated!!

--- frontend/Amb Engineering Company/src/Component/Layout/Footer.jsx
+++ frontend/Amb Engineering Company/src/Component/Layout/Footer.jsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
+import axios from "axios";
+import { Backend_Url } from "../utils/utils";
+import { Link, NavLink } from 'react-router-dom';
 
 
 const Footer = () => {
+
+  const [logourl, setlogourl] = useState("")
+
+  const fetchurllogoimg2 = async () => {
+      // const publicid = "wall-clading_j3yzwq";
+      const publicid = "WhatsApp_Image_2025-03-11_at_8.35.37_PM_xvo73k";
+  
+      try {
+        const res = await axios.get(
+          `${Backend_Url}/get-img-url`,
+          // "http://localhost:8086/api/userEmail/get-img-url",
+          {
+            params: { public_id: publicid },
+          }
+        );
+        setlogourl(res.data.url);
+        // window.open(res.data.url, "_blank"); // Open the PDF automatically
+      } catch (error) {
+        console.error("Error fetching the PDF URL:", error);
+        alert("Failed to fetch the PDF URL. Please try again.");
+      }
+    };
+  
+    useEffect(() => {
+      fetchurllogoimg2();
+    }, []);
   return (
     <div className="mt-9 bg-gray-800 text-white py-8">
       <div className=" px-4  flex max-w-7xl mx-auto">
+        <Link to="/">
         <img
-          src="https://m.media-amazon.com/images/I/51YJsBTSFSL.jpg"
-          className="rounded-full border border-4 border-blue-400 w-38 h-43 sm:w-45"
+          // src="https://m.media-amazon.com/images/I/51YJsBTSFSL.jpg"
+          src= {logourl}
+          className="rounded-full   object-fill  w-65 h-43 sm:w-55 sm:h-45"
           height={200}
           width={200}
-          alt=""
+          alt="My logo for amb engineering company and go to home"
         />
+        </Link>
 
         <div className="w-full text-center">
           <h2 className="text-2xl font-semibold mb-4">
